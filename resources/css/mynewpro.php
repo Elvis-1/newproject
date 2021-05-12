66521e0c09dfaad3f1d0f9dc97b0d4c8e9a4dfef
Elvis <e.igiebor@cyberticks.com>
2021-05-12 22:00:12 +0100

    added model and controller

--- resources/css/mynewpro.php
+++ resources/css/mynewpro.php
@@ -1,6 +1,10 @@
 <?php
-/* ----
-1) 
+/* ---- STARTED A NEW PROJECT
+composer create-project laravel/laravel newproject
+1) $ composer reqiure laravel-frontend-presets/tailwindcss --dev
+2) npm install and npm run dev
+3) create model with migration -- $ php artisan make:model Car -m
+4) make controller with resource -- $ php artisan  make:controller CarsController --resource
 
 
 
